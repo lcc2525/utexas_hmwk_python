05d48a80065926a51a60403175de01bec1debace
LCC2525 <christian.coleman3@g.austincc.edu>
2018-08-28 03:07:47 -0500

    remove keys

--- tweetproject/configtwit.py
+++
@@ -1,6 +0,0 @@
-consumer_key = "TQi6EPa4sfDDLJhxoLvKZ45Sx"
-consumer_secret = "LOfaGua49SwO14NSsvf2L3XbQGeVnxCVoJBPZX6MZa2wPrwPlB"
-access_token = "1029260403073839104-f1tkdITaKxZkM94IleDppnH9SW8yBT"
-access_token_secret = "Ux7g8aeEYrlwWKYth176tVRzopEaD7xv5cV6bcJlZBP6D"
- 
- 
