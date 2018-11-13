86a49470f635c6c0eefe8d7365dc7ab08872e1d2
Christian Coleman <40559147+lcc2525@users.noreply.github.com>
2018-11-13 15:38:39 -0600

    Add files via upload
    correct app.js file

--- ufohomework/static/js/app.js
+++ ufohomework/static/js/app.js
@@ -1,18 +1,39 @@
-//from data.js
-// var tableData = data;
-// var select = document.getElementById("datetime"); 
-var array = ["78610","78613","78617","78641","78652","78653","78660","78664","78681","78701","78702","78703","78704","78705","78712","78717","78719","78721","78722","78723","78724","78725","78726","78727","78728","78729","78730","78731","78732","78733","78734","78735","78736","78737","78738","78739","78741","78742","78744","78745","78746","78747","78748","78749","78750","78751","78752","78753","78754","78756","78757","78758","78759"]; 
+// from data.js
+var tableData = data;
+var tbody = d3.select("tbody");
 
-var myDiv = document.getElementById("mySelect");
-//Create and append select list
-var selectList = document.createElement("value");
-// selectList.id = "mySelect";
-myDiv.appendChild(selectList);
+// select the submit button using d3 selectors
+var submit = d3.select("#filter-btn");
+// Create click on function, need several inputs for multiple variables
+submit.on('click', function() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var inputElement = d3.select("#city");
+    var inputValue2 = inputElement.property("value");
+    var inputElement = d3.select("#state");
+    var inputValue3 = inputElement.property("value");
+    var inputElement = d3.select("#country");
+    var inputValue4 = inputElement.property("value");
+    var inputElement = d3.select("#shape");
+    var inputValue5 = inputElement.property("value");
+    // console.log(inputValue, inputElement);
 
-//Create and append the options
-for (var i = 0; i < array.length; i++) {
-    var option = document.createElement("option");
-    option.value = array[i];
-    option.text = array[i];
-    selectList.appendChild(option);
-};
+    // filterd table by only entered parameters
+    var filteredData = (inputValue !== "") ? (tableData.filter(sighting => sighting.datetime === inputValue)) : tableData;
+    var filteredData2 = (inputValue2 !== "") ? (filteredData.filter(sighting => sighting.city === inputValue2)) : filteredData;
+    var filteredData3 = (inputValue3 !== "") ? (filteredData2.filter(sighting => sighting.state === inputValue3)) : filteredData2;
+    var filteredData4 = (inputValue4 !== "") ? (filteredData3.filter(sighting => sighting.country === inputValue4)) : filteredData3;
+    var filteredData5 = (inputValue5 !== "") ? (filteredData4.filter(sighting => sighting.shape === inputValue5)) : filteredData4;
+    // console.log(filteredData, filteredData2, filteredData3, filteredData4);
+    // console.log(inputValue2, inputValue3, inputValue5)
+
+    // Print filterd data into table
+    filteredData5.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+});
